838e36597dc96c00cd84db56552d390cc55f2f01
DFS <2601664329@qq.com>
2023-01-11 16:15:15 +0800

    select-epoll code

---
+++ sep_clnt.cpp
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+const int BUF_SIZE = 1024;
+
+int main(int argc, char **argv){
+    int sock;
+    char buf[BUF_SIZE];
+    struct sockaddr_in serv_addr;
+
+    FILE* readfp;
+    FILE* writefp;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
+    readfp = fdopen(sock, "r");
+    writefp = fdopen(sock, "w");
+    
+    while(1){
+        if(fgets(buf,sizeof(buf),readfp) == NULL){
+            break;
+        }
+        fputs(buf,stdout);
+        fflush(stdout);
+    }
+
+    fputs("FROM CLIENT: Thank you \n",writefp);
+    fflush(writefp);
+    fclose(writefp);
+    fclose(readfp);
+}
